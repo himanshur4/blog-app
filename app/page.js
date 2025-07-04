2d4280ada95045e5afce6add40a0c0fc0ca70ee8
Himanshu Raj <hraj808404@gmail.com>
2025-07-05 00:36:10 +0530

    feat: initialize Next.js project with Tailwind CSS and ESLint
    - Added package.json with dependencies for React, Next.js, Tailwind CSS, and ESLint.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets for icons and logos in the public directory.

---
+++ app/page.js
@@ -0,0 +1,13 @@
+'use client';
+import BlogItem from "@/Components/BlogItem";
+import Header from "@/Components/Header";
+
+
+export default function Home() {
+  return (
+   <>
+    <Header/>
+    <BlogItem/>
+    </>
+  );
+}
